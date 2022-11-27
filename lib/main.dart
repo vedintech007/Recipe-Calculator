ced2cd2057c436e01dcfd3ad05030c8080a499f0
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2022-11-27 23:59:30 +0000

    done styling main recipe pages

--- lib/main.dart
+++ lib/main.dart
@@ -45,11 +45,26 @@ class HomePage extends StatelessWidget {
   // TODO: Add buildRecipeCard() here
   Widget buildRecipeCard(RecipeModel recipe) {
     return Card(
-      child: Column(
-        children: [
-          Image.asset(recipe.imageUrl),
-          Text(recipe.label),
-        ],
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image.asset(recipe.imageUrl),
+            const SizedBox(height: 14.0),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
